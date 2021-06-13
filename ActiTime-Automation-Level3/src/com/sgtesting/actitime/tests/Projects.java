86a147147f920a8cf3f121a8cfd0a84d8020a086
subramanyamp-95 <subramanyamp95@gmail.com>
2021-06-13 06:33:06 +0530

    Modified-ActiTime-Automation-Level3

--- ActiTime-Automation-Level3/src/com/sgtesting/actitime/tests/Projects.java
+++ ActiTime-Automation-Level3/src/com/sgtesting/actitime/tests/Projects.java
@@ -1,5 +1,6 @@
 package com.sgtesting.actitime.tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Projects {
@@ -19,6 +20,13 @@ public class Projects {
 	{
 		try
 		{
+			
+			oBrowser.findElement(By.xpath("//div[@class='addNewContainer']//div[@class='addNewButton']//div[@class='title ellipsis']")).click();
+			Thread.sleep(1000);
+			oBrowser.findElement(By.xpath("//div[@class='item createNewProject ellipsis']")).click();
+			Thread.sleep(1000);			
+			oBrowser.findElement(By.xpath("//input[@id='projectPopup_projectNameField']")).sendKeys("New-Project");Thread.sleep(1000);	
+			oBrowser.findElement(By.xpath("//div[@id='projectPopup_footer']//span[@class='buttonTitle']")).click();Thread.sleep(1000);
 			
 		}catch(Exception e)
 		{
@@ -41,6 +49,12 @@ public class Projects {
 	{
 		try
 		{
+			oBrowser.findElement(By.xpath("//div[@class='node projectNode selected']//div[@class='editButton available']")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//textarea[@class='textarea'][@placeholder='Enter project description...']")).sendKeys("Project description");
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//div[@class='edit_project_sliding_panel sliding_panel']//div[@class='closeButton']")).click();
+			Thread.sleep(2000);
 			
 		}catch(Exception e)
 		{
@@ -63,6 +77,15 @@ public class Projects {
 	{
 		try
 		{
+			
+			oBrowser.findElement(By.xpath("//div[@class='node projectNode selected']//div[@class='editButton available']")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//div[@class='edit_project_sliding_panel sliding_panel']//div[@class='action']")).click();
+			Thread.sleep(1000);
+			oBrowser.findElement(By.xpath("//div[@class='edit_project_sliding_panel sliding_panel']//div[@class='deleteButton']")).click();
+			Thread.sleep(1000);
+			oBrowser.findElement(By.xpath("//span[@id='projectPanel_deleteConfirm_submitTitle']")).click();
+			Thread.sleep(4000);
 			
 		}catch(Exception e)
 		{
